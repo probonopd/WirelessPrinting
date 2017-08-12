bc80a205fc50b4e1fad281d7b00cbea70b3bd05e
probonopd <probonopd@users.noreply.github.com>
2017-08-12 15:33:14 +0200

    Update ESP8266WirelessPrintAsync.ino

--- ESP8266WirelessPrintAsync/ESP8266WirelessPrintAsync.ino
+++ ESP8266WirelessPrintAsync/ESP8266WirelessPrintAsync.ino
@@ -382,7 +382,8 @@ void setup() {
 
   server.on("/api/job", HTTP_GET, [](AsyncWebServerRequest * request) {
     // http://docs.octoprint.org/en/master/api/datamodel.html#sec-api-datamodel-jobs-job
-    request->send(200, "application/json", "{\r\n  \"job\": {\r\n    \"file\": {\r\n      \"name\": \"Unknown\",\r\n      \"origin\": \"local\",\r\n      \"size\": 1468987,\r\n      \"date\": 1378847754\r\n    },\r\n    \"estimatedPrintTime\": 8811,\r\n    \"filament\": {\r\n      \"length\": 810,\r\n      \"volume\": 5.36\r\n    }\r\n  },\r\n  \"progress\": {\r\n    \"completion\": 0.2298468264184775,\r\n    \"filepos\": 337942,\r\n    \"printTime\": 0,\r\n    \"printTimeLeft\": 0\r\n  }\r\n}");
+    float percentage = filesize_read / filesize; # Not super accurate but what OctoPrint does, too
+    request->send(200, "application/json", "{\r\n  \"job\": {\r\n    \"file\": {\r\n      \"name\": \"Unknown\",\r\n      \"origin\": \"local\",\r\n      \"size\": " + String(filesize) + ",\r\n      \"date\": 1378847754\r\n    },\r\n    \"estimatedPrintTime\": 8811,\r\n    \"filament\": {\r\n      \"length\": 810,\r\n      \"volume\": 5.36\r\n    }\r\n  },\r\n  \"progress\": {\r\n    \"completion\": " + String(percentage) + ",\r\n    \"filepos\": " + String(filesize_read) + ",\r\n    \"printTime\": 0,\r\n    \"printTimeLeft\": 0\r\n  }\r\n}");
   });
   // TODO: Implement POST. Cura uses this to pause and abort prints.
 
