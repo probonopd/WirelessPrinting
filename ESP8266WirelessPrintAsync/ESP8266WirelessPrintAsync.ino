67358ca1d558abd2eead5828b3c39fff5fa5e95a
probonopd <probonopd@users.noreply.github.com>
2017-08-12 16:39:32 +0200

    Update ESP8266WirelessPrintAsync.ino

--- ESP8266WirelessPrintAsync/ESP8266WirelessPrintAsync.ino
+++ ESP8266WirelessPrintAsync/ESP8266WirelessPrintAsync.ino
@@ -208,7 +208,7 @@ void handlePrint() {
         if (pos != -1) {
           line = line.substring(0, pos);
         }
-        if (((line.startsWith("(")) || (line.startsWith(";")) || (line.startsWith("\r"))) {
+        if ((line.startsWith("(")) || (line.startsWith(";")) || (line.startsWith("\r"))) {
           continue;
         }
         sendToPrinter(line);
